e2c8290e23bcf66c56e543a2b907b35ae50a6e1b
kanishkcoe <32832273+kanishkcoe@users.noreply.github.com>
2018-02-07 20:53:50 +0530

    for loop added

--- ascii.cpp
+++ ascii.cpp
@@ -7,13 +7,13 @@ int main()
   char ch = 'A';
   int num = ch;
 
-  cout << "The ASCII code for " << ch << "is " << num << endl;
-  cout << "Adding 1 to the character code" << endl;
-
-  ch = ch + 1;
-  num = ch;
-
-  cout << "The ASCII code for " << ch << "is " << num << endl;
+  for(int i = 0; i < 26; i++)
+  {
+    cout << "The ASCII code for " << ch << " is " << num << endl;
+    cout << "Adding 1 to the character code" << endl;
+    ch = ch + 1;
+    num = ch;
+  }
 
   return 0;
 }
