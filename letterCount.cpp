65491a7342cec05dd3dd5f79ff0489a141ddc137
kanishkcoe <32832273+kanishkcoe@users.noreply.github.com>
2018-02-11 12:59:35 +0530

    kunal's code

---
+++ letterCount.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  char s[10];
+  char x;
+  int count;
+  cin.getline(s, 10);
+
+  for(int i = 0; s[i] != NULL; i++)
+  {
+    count = 0;
+    if(s[i] != '*')
+    {
+      x = s[i];
+      for(int j = 0; s[j] != NULL; j++)
+      {
+        if(x == s[j])
+        {
+          count++;
+          s[j] = '*';
+        }
+      }
+    }
+    if(count != 0)
+      cout << x << " : " << count << endl;
+  }
+
+  return 0;
+}
