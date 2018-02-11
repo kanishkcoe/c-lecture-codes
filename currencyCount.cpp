3948d7434a401935b5b004ad0a39243f518a88ae
kanishkcoe <32832273+kanishkcoe@users.noreply.github.com>
2018-02-11 12:37:09 +0530

    2 new codes

---
+++ currencyCount.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int count = 0;
+  int countPresest = 0;
+  int currency[] = {2000, 500, 200, 100, 50, 20, 10, 5, 2, 1};
+  int amount;
+
+  cout << "Enter amount : "; cin >> amount;
+
+  for(int i = 0; i < 10; i++)
+  {
+    countPresest = amount / currency[i];
+    count += amount / currency[i];
+    cout << currency[i] << " notes : " << countPresest << endl;
+    amount %= currency[i];
+  }
+
+  cout << "Currency notes required = " << count << endl;
+  return 0;
+}
---
+++ permutation.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+void swap(char *x, char *y)
+{
+    char temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void permute(char *a, int l, int r)
+{
+   int i;
+   if (l == r)
+      cout << a;
+   else
+   {
+       for (i = l; i <= r; i++)
+       {
+          swap((a+l), (a+i));
+          permute(a, l+1, r);
+          swap((a+l), (a+i)); //backtrack
+       }
+   }
+   cout << endl;
+}
+
+int main()
+{
+  char str[] = "ABC";
+  int n = strlen(str);
+  permute(str, 0, n-1);
+  return 0;
+}
