64fa476ac07eec2364dcf8a54817e3acd06f0cc2
kanishkcoe <32832273+kanishkcoe@users.noreply.github.com>
2018-02-13 19:58:14 +0530

    code

---
+++ stringPalindrome.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+  char s[20];
+  int flag = 1;
+  cin.getline(s, 20);
+
+  for(int i = 0, l = strlen(s) - 1; i < l; i++, l--)
+  {
+    if(s[i] != s[l])
+    {
+      flag = 0;
+    }
+  }
+
+  if(flag == 0)
+  {
+    cout << "Not palindrome" << endl;
+  }
+  else
+  {
+    cout << "palindrome" << endl;
+  }
+
+  return 0;
+}
